8317615ced9ee20b262be9dc5ed3125aad8d94f5
Nicolás Chicaiza <jefryn@unicauca.edu.co>
2023-09-30 17:20:18 -0500

    :construction: (NICO-11) Trabajando en plantilla

--- docker-NSAC2023-demo/react_app/src/App.tsx
+++ docker-NSAC2023-demo/react_app/src/App.tsx
@@ -1,23 +1,8 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
-import { TasksPage } from "./pages/TasksPage"
-import { TaskFormPage } from "./pages/TaskFormPage"
-import { Navigation } from "./components/Navigation"
-import { Toaster } from "react-hot-toast"
+import { Template } from "./template/Template"
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="container mx-auto">
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Navigate to="/tasks" />}></Route>
-          <Route path="/tasks" element={<TasksPage />}></Route>
-          <Route path="/tasks/create" element={<TaskFormPage />}></Route>
-          <Route path="/tasks/:id" element={<TaskFormPage />}></Route>
-        </Routes>
-        <Toaster />
-      </div>
-    </BrowserRouter>
+    <Template />
   )
 }
 
